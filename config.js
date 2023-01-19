60e0d3fee9ca0c29d43dd0812d5e9fca351b4e27
Alberto <alberto.cabello64@gmail.com>
2023-01-19 20:22:22 -0300

    Config

--- config.js
+++ config.js
@@ -15,7 +15,8 @@ const mysqlConnetion = mysql.createConnection({
     host: process.env.DB_HOST,
     user : process.env.DB_USER ,
     password : process.env.DB_PASSWORD,
-    database : process.env.DB_DATABASE 
+    database : process.env.DB_DATABASE || 'rlb182',
+    puerto : process.env.DB_PUERTO || 3306 
     
 });
 
